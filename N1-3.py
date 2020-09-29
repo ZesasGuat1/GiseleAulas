c82169df6ac98e8345763a47c7ca15bde4dca645
ZesasGuat1 <64496677+ZesasGuat1@users.noreply.github.com>
2020-09-29 13:09:45 -0300

    Create N1-3.py

---
+++ N1-3.py
@@ -0,0 +1,49 @@
+while True:
+    pergunta = int(input("Digite uma nota entre 0 e 10: "))
+
+    if pergunta == 0:
+        print("Nota 0 cadastrada")
+        break
+
+    elif pergunta == 1:
+        print("nota 1 cadastrada")
+        break
+
+    elif pergunta == 2:
+        print("nota 2 cadastrada")
+        break
+
+    elif pergunta == 3:
+        print("nota 3 cadastrada")
+        break
+
+    elif pergunta == 4:
+        print("nota 4 cadastrada")
+        break
+
+    elif pergunta == 5:
+        print("nota 5 cadastrada")
+        break
+
+    elif pergunta == 6:
+        print("nota 6 cadastrada")
+        break
+
+    elif pergunta == 7:
+        print("nota 7 cadastrada")
+        break
+
+    elif pergunta == 8:
+        print("nota 8 cadastrada")
+        break
+
+    elif pergunta == 9:
+        print("nota 9 cadastrada")
+        break
+
+    elif pergunta == 10:
+        print("nota 10 cadastrada")
+        break
+
+    else:
+        print("Digite um numero valido")
